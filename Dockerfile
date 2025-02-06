b4e3504c69be2f9f7ff2372ffc1a94f7de68127c
chickenwingtp <oceantp2023@gmail.com>
2025-02-07 02:27:31 +0800

    fixing pm2

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ COPY . .
 
 RUN npm install
 RUN npm install pm2 -g
+RUN pm2 install pm2-server-monit
+
+# Add PM2 monitoring setup
+ENV PM2_PUBLIC_KEY="q2pwmmbba6wgs9k"
+ENV PM2_PRIVATE_KEY="fo46q2hqe29noqy"
+
 RUN npm run build
 
 EXPOSE 3000
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,6 +15,14 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 3000,
       NODE_NO_WARNINGS: '1'
-    }
+    },
+    instances: 1,
+    merge_logs: true,
+    post_update: ["npm install"],
+    monitoring: true,
+    instance_var: 'INSTANCE_ID',
+    pmx: true,
+    trace: true,
+    deep_monitoring: true
   }]
 }
